9fd478d1bf696c23ccd95ff646aa16d6c419de72
Tiago Luiz Fernandes <tiagoluiz@gft.com>
2023-02-01 15:25:31 -0300

    review

--- conjunto-bom-ruim/src/main/java/Main.java
+++ conjunto-bom-ruim/src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Tiago Luiz Fernandes
@@ -10,25 +13,34 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int line = 0;
+        int numberOflines = 0;
 
-        line = Integer.parseInt(br.readLine());
+        numberOflines = Integer.parseInt(bufferedReader.readLine());
 
-        while (line != 0 ) {
-            boolean found = false;
+        while (numberOflines != 0 ) {
+            String listType = "Conjunto Bom";
+            List<String> stringList = new ArrayList<>();
 
-            for(int i = 0; i <= originalWord.length() -1; i++) {
-                if(originalWord.substring(0,i).endsWith(originalWord.substring(i))) {
-                    System.out.println(originalWord.substring(0,i));
-                    found = true;
+            for(int x = 0; x < numberOflines; x++) {
+                String palavra = bufferedReader.readLine();
+                stringList.add(palavra);
+            }
+
+            stringList = stringList.stream().sorted().collect(Collectors.toList());
+
+            for (int x = 0; x < numberOflines - 1; x++) {
+                if (stringList.get(x + 1).startsWith(stringList.get(x))) {
+                    listType = "Conjunto Ruim";
+                    break;
                 }
             }
 
-            if(!found) {
-                System.out.println(originalWord);
-            }
+            System.out.println(listType);
+
+            numberOflines = Integer.parseInt(bufferedReader.readLine());
+
         }
     }
 }
